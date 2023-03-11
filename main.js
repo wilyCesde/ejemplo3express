3cf3b8f9157797ccdd4d89fd9f59d629e94f7722
wilyCesde <wruizza@cesde.net>
2023-03-11 15:43:19 -0500

    comentarios

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 //const express = require('express');
+// npm init -y
+
+// code .
+
+// npm install express --save
+
+// npm i nodemon -D
+
+ //"type":"module", se usa en el scrip de pachage json para trabajra import
+
 
 import express from 'express';
 
@@ -6,7 +16,27 @@ const app = express();
 
 const port = 1500;
 
-app.listen(port,()=>{
-    console.log(`server in http://localhost:${port}`)
+
+// primer metodo para que el server escuche 
+app.listen(port, () => {
+  console.log(`server in http://localhost:${port}`)
+})
+
+// Endpoinst
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+  res.send('Got a POST request')
+})
+
+app.put('/user', (req, res) => {
+  res.send('Got a PUT request at /user')
+})
+
+app.delete('/user', (req, res) => {
+  res.send('Got a DELETE request at /user')
 })
 
